60ef86a032117c22dfe104fc411702f8ead051c5
AtiyyahRawat <atiyyah.rawat@gmail.com>
2021-09-08 12:40:36 +0100

    Set API tags based on API data tags

--- cypress/integration/previewApi.spec.js
+++ cypress/integration/previewApi.spec.js
@@ -21,24 +21,31 @@ describe("Preview an API", () => {
     });
 
     it("View active status tag", function () {
-        cy.get(".apiPreview").find(".title > span.lbh-tag").first().should(($tag) => {
+        const expectedClass = this.apiData.tags.includes("Active") ? "lbh-tag" : "lbh-tag--grey";
+        cy.get(".apiPreview").find(".title > span.govuk-tag").first().should(($tag) => {
             const tagText = $tag.text();
-            const expectedTagText = this.apiData.tags.includes("active") ? "Active" : "Inactive";
+            const expectedTagText = this.apiData.tags.includes("Active") ? "Active" : "Inactive";
             expect(tagText).to.equal(expectedTagText);
-        });
+        }).should('have.class', expectedClass);
     });
 
     it("View environment status tags", function () {
+        const expectedEnvTagsNo = 3;
         cy.get(".apiPreview").find(".tags").first().children()
-            .should('have.length', this.apiData.tags.length )
+            .should('have.length', expectedEnvTagsNo)
             .each((tag) => {
-                expect(tag.text()).to.be.oneOf(this.apiData.tags);
+                if (this.apiData.tags.includes(tag.text())){
+                    expect(tag).to.have.class("lbh-tag--green");
+                } else {
+                    expect(tag).to.have.class("lbh-tag--grey");
+                }
             });
     });
 
     it("When the user clicks on an API name they are directed to the SwaggerHub page", function() {
-        cy.get(".apiPreview").find("h3").first().click();
-        expect(cy.url()).to.equal(this.apiData.properties[0].url);
+        cy.get(".apiPreview").find("a").first().click();
+        const expectedUrl = this.apiData.properties[0].url.replace("api", "app");
+        cy.url().should('include', expectedUrl);
     });
 
 });
--- src/components/apiPreview/apiPreview.component.jsx
+++ src/components/apiPreview/apiPreview.component.jsx
@@ -1,17 +1,20 @@
 import React from "react";
 
-const ApiPreview = ({name, description}) => {
+const ApiPreview = ({name, description, tags, properties}) => {
+
+  const urlData = properties.filter( property => property.type === "Swagger")[0];
+
   return (
     <li className="apiPreview">
       <div className="title">
-        <h3>{name}</h3>
-        <span className="govuk-tag lbh-tag">Active/Inactive</span>
+        <a href={urlData.url.replace("api", "app")} className="lbh-link"><h3>{name}</h3></a>
+        <span className={`govuk-tag lbh-tag${tags.includes("Active") ? "" : "--grey"}`}>{ tags.includes("Active") ? "Active" : "Inactive" }</span>
       </div>
       <p>{description}</p>
       <div className="tags">
-        <span className="govuk-tag lbh-tag lbh-tag--green">Development</span>
-        <span className="govuk-tag lbh-tag lbh-tag--green">Staging</span>
-        <span className="govuk-tag lbh-tag lbh-tag--green">Production</span>
+        <span className={`govuk-tag lbh-tag lbh-tag${tags.includes("Development") ? "--green" : "--grey"}`}>Development</span>
+        <span className={`govuk-tag lbh-tag lbh-tag${tags.includes("Staging") ? "--green" : "--grey"}`}>Staging</span>
+        <span className={`govuk-tag lbh-tag lbh-tag${tags.includes("Production") ? "--green" : "--grey"}`}>Production</span>
       </div>
     </li>
   );
