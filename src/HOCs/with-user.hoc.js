daa78bbada1b618869e259bd64a0f61174ae6604
LBHLbasiu <livia.basiu@gmail.com>
2021-09-10 12:37:43 +0100

    Deleted for merge

--- src/HOCs/with-user.hoc.js
+++
@@ -1,8 +0,0 @@
-import React, { useState, useContext } from "react";
-import UserContext from "../context/user.context.js";
-
-const withUser = Component => props => (
-  <Component currentUser={useContext(UserContext)} {...props} />
-);
-
-export default withUser;
