ba85a191272597c623623f5be8b62cf7ee82ea3d
AtiyyahRawat <atiyyah.rawat@gmail.com>
2021-09-13 14:33:58 +0100

    Set up CircleCI

---
+++ terraform/production/main.tf
@@ -0,0 +1,39 @@
+provider "aws" {
+  region  = "eu-west-2"
+  version = "~> 2.0"
+}
+
+terraform {
+  backend "s3" {
+    bucket  = "terraform-state-production-apis"
+    encrypt = true
+    region  = "eu-west-2"
+    key     = "services/developer-hub-frontend/state"
+  }
+}
+
+resource "aws_s3_bucket" "frontend-bucket-production" {
+  bucket = "lbh-developer-hub-production.hackney.gov.uk"
+  acl    = "private"
+  versioning {
+    enabled = true
+  }
+  website {
+    index_document = "index.html"
+    error_document = "index.html"
+  }
+}
+
+module "cloudfront-production" {
+  source = "github.com/LBHackney-IT/aws-hackney-common-terraform.git//modules/cloudfront/s3_distribution"
+  s3_domain_name = aws_s3_bucket.frontend-bucket-production.bucket_regional_domain_name
+  origin_id = "developer-hub-frontend"
+  s3_bucket_arn = aws_s3_bucket.frontend-bucket-production.arn
+  s3_bucket_id = aws_s3_bucket.frontend-bucket-production.id
+  orginin_access_identity_desc = "Developer Hub frontend cloudfront identity"
+  cname_aliases = []
+  environment_name = "production"
+  cost_code= "B0811"
+  project_name= "Developer Hub"
+  use_cloudfront_cert = true
+}
