68cc5543f4cc97978a992e5d7b64fdbc89296f3b
LBHLBasiu <livia.basiu@hackney.gov.uk>
2021-10-13 10:06:36 +0100

    home page update
    content added
    sidebar

--- src/pages/home-pages/home-pages.pages.jsx
+++ src/pages/home-pages/home-pages.pages.jsx
@@ -1,3 +1,8 @@
+import React from 'react';
+import diagramA from './diagramA.png';
+import diagramB from './diagramB.png'
+
+
 export const HomePage1 = () => {
   return (
     <div className="main-container">
@@ -32,7 +37,65 @@ export const HomePage1 = () => {
 export const HomePage2 = () => {
   return (
     <div className="main-container">
-      Page 2
+      <h2>The need of a Developer Hub</h2><br/>
+
+    <h3> As a Developer</h3>
+        <ul>
+          <li>As a developer I would like consistency between the developer hub and swagger so that any changes to the APIs can be reflected straight away. </li>
+          <li>As a developer I want a way to easily find APIs so that I can use these APIs effectively as well as stay up to date with any changes that might come up .</li>
+          <li>As a developer I need to make sure that access to the APIs  is secure so that only authorised users can make requests to Hackney APIs.</li>
+          <li>As a developer I would like to see what APIs are available at HackIT to be used so that I can choose the ones I need for the service development.</li>
+          <li>As a developer I would like to build and reuse the Organisations APIs so that development efforts can focus on other requirements </li>
+          <li>As a user I can log in with my google account associated with Hackney, so that my identity can easily be verified and my permissions can be managed effectively and efficiently. </li>
+        </ul>
+
+      <h3>As a Service User </h3>
+        <ul>
+          <li> I want the ability to view the details of the results returned in a search so that I can easily look through the different results and make the correct selection.</li>
+          <li>I want to be able to register for the application by entering my name, email address, my password and confirming my password so that I can feel secure. </li>
+          <li>As a user I can log in into the application by entering my email and chosen password  so that I can feel my account is being secure </li>
+          <li>As a user I can reset my password if i have forgotten my password </li>
+          <li>As a user I can see my personal information and the APIs used on my profile </li>
+          <li>I can edit my details such as the email address so that I can continue using the same account </li>
+          <li>As a service user I would like to see the list of APIs available in the catalogue so that I can make an informed decision for the product development.</li>
+        </ul>
+
+        <h3>As an Aplication Support Analyst</h3>
+          <ul>
+            <li>As an application support analyst, I would like to know what endpoints and functionality an API has, so that I can familiarise myself with the types of support requests I may get.</li>
+            <li> I need to understand the queries being used by the Developers Hub  so that I can deal with support requests accordingly and resolve the potential issues in the underlying data.</li>
+          </ul>
+
+          <h3>As a Data Analyst</h3>
+            <ul>
+              <li>I need to connect to the API so that data is easy to interpret and available for further reporting purposes and analysis.</li>
+              <li>As a user, I would like to see Data in a format that's structured e.g. csv or JSON, so that I can easily manipulate the data.</li>
+              <li>I would like to have data in a format that can be linked into programmes such as Tableau or Qlik sense so that I can create reports and data visualisation</li>
+            </ul>
+
+          <h3>As a Solution Architect</h3>
+            <ul>
+              <li>As a solution architect I would like to view all APIs available at HackIT so that I can ensure that they are consistent with HackIT development standards. </li>
+              <li>As a solution architect I would like to view all APIs and endpoints available at HackIT so that I can reuse APIs across many projects. </li>
+              <li>As a solution architect, I would like to ensure that the data shown in mock APIs is placeholder data in line with GDPR compliance, so that sensitive data is not compromised. </li>
+            </ul>
+
+          <h3> As a QA Analyst</h3>
+            <ul>
+              <li>As a tester I would like to view use cases so that I can test the required functionality. </li>
+              <li>As at tester I would like to view all API Endpoints so that I can complete thorough E2E testing. </li>
+              <li>I would like to be able to view all existing APIs and endpoints to ensure the final product of the API I am testing meets the organisation standards. </li>
+          </ul>
+
+          <h3>As a Cyber Silver Member</h3>
+            <ul>
+              <li>As a member of Cyber Silver, I would like to ensure nobody can access live data through the APIs without being registered and logged in, so that cybersecurity is not compromised. </li>
+            </ul>
+
+          <h3> As a Product Owner</h3>
+            <ul>
+              <li>As a product owner, I want to confirm that the data from an API is correct, based on the business area understanding of the wider team and any prior discovery work completed.</li>
+            </ul>
     </div>
   );
 };
@@ -40,7 +103,45 @@ export const HomePage2 = () => {
 export const HomePage3 = () => {
   return (
     <div className="main-container">
-      Page 3
+      <h3>API Specifications </h3>
+      <br/>
+        <a href = "https://playbook.hackney.gov.uk/api-specifications/ ">You can find our API Specification Playbook here</a>
+        <br/><br/>
+
+    <h3>Why you might need an API</h3>
+        <br/>
+      <ul>
+        <li>Sharing your data - Secure, structured and real-time access to data can be provided by an API. </li>
+        <li>Data integrity - Providing a single source of data in the same format means more consistency across different systems. </li>
+        <li>Integrations - APIs can provide links between different back end systems. </li>
+      </ul>
+
+      <h3>What is an API?</h3>
+
+      <p>Each time you tell your smart speaker to "turn on the living room light" the speaker will send an instruction to an API.
+        The API will then send a series of instructions in the background which eventually culminates in a bulb switching on.
+        You didn't need to write any complicated code or give any specifics about what "turn on" means or exactly where "living room light" is located.
+        All of this information was dealt with inside of the API.
+        Wherever possible APIs shouldn't care what is passing it an instruction.
+        Using an app on your smart phone to turn on your living room lights will use the same API and send the exact same instructions as the smart speaker did.
+        The API should behave in the same way to give the same end result.
+        Therefore an API is a resource that provides a simple way to communicate between two devices or pieces of software.</p>
+
+      <img src={diagramA} width="650" height="320"/> <br/><br/>
+
+      <h3>What is an API Platform?</h3>
+
+      <p> There is more to APIs than just sending a command and the API responding.
+          You obviously don't want anyone on the internet to be able to turn your living room lights on.
+          You need to be able decide who is allowed to turn the lights on.
+          Using an API Gateway we are able to issue Tokens to people who want to use our APIs.
+          Each time a developer attempts to access our APIs they will provide this token, essentially telling us who they are.
+          Requests to our APIs that do not provide the token will be rejected.</p>
+
+        <img src={diagramB} width="650" height="320" align="centre"/>
+
+      <p>You may also want to ensure that someone cannot bombard your API
+         with thousands of calls over a very short period of time. This could cost you time and resources.</p>
     </div>
   );
 };
@@ -48,7 +149,15 @@ export const HomePage3 = () => {
 export const HomePage4 = () => {
   return (
     <div className="main-container">
-      Page 4
+        <h3> Our Ways of Working</h3>
+
+        <p>All HackIT developed code should meet the <a href= "https://github.com/LBHackney-IT/Hackney-Development-Standards">12 Hackney Development Standards.</a>
+           around the world to ensure that HackIT APIs are consistent,supportable and of high quality.</p>
+
+         <p>All newly created HackIT APIs will be developed using the guidelines in the HackIt API playbook.
+            It provides clear guidance on the standards that are being followed when developing new APIs
+            and is also used whenever amending old APIs which were created prior to the existence of the Playbook.
+            Both of these guides are constantly evolving over time alongside technological changes.</p>
     </div>
   );
 };
