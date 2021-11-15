dfc288c896de23acb05f3557f22168242d387882
elena-evans <elena.evans@hackney.gov.uk>
2021-11-15 09:39:05 +0000

    moved welcome above links, restructured header to match other services

---
+++ cypress/integration/homepage.spec.js
@@ -0,0 +1,8 @@
+// describe("Sign out functionality", () => {
+//   beforeEach(function () {
+//       cy.visit('/');
+//   });
+//     it("Has a link to the homepage in the header", () => {
+//       cy.contains("HOME");
+//     });
+// });
