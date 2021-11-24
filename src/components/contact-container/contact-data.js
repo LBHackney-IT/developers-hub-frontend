6d5faa778c5fed5de1121ca7b528624484cf5f5a
LBHLBasiu <livia.basiu@hackney.gov.uk>
2021-11-24 11:39:03 +0000

    contact list updated

--- src/components/contact-container/contact-data.js
+++ src/components/contact-container/contact-data.js
@@ -4,7 +4,6 @@ const CONTACT_DATA = [
   "Selwyn Preston[selwyn.preston@hackney.gov.uk]-Lead Developer",
   "Matthew Keyworth[matthew.keyworth@hackney.gov.uk]-Lead Developer",
   "Dave Dean[dave.dean@hackney.gov.uk]-Lead Developer",
-  "Bukky Omotoso[Bukky.Omotoso@hackney.gov.uk]-Lead Developer",
   "Livia Basiu[livia.basiu@hackney.gov.uk]-Maintainer and Collaborator",
   "Atiyyah Rawat[atiyyah.rawat@hackney.gov.uk]-Maintainer and Collaborator",
   "Humairaa Mulla[Humairaa.Mulla@hackney.gov.uk]-Maintainer and Collaborator",
