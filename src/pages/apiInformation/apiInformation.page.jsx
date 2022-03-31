63041203102d7dd7ca560fc0b160c095bfbba38e
lbhMichael <michael.ndukwe@hackney.gov.uk>
2022-03-31 16:42:26 +0100

    add get interaction with api

--- src/pages/apiInformation/apiInformation.page.jsx
+++ src/pages/apiInformation/apiInformation.page.jsx
@@ -1,8 +1,8 @@
 import { React, useEffect, useState } from "react";
 import { useLocation, useParams } from "react-router";
 import axios from "axios";
-import Cookies from 'js-cookie';
-import Skeleton from 'react-loading-skeleton';
+import Cookies from "js-cookie";
+import Skeleton from "react-loading-skeleton";
 
 import withUser from "../../HOCs/with-user.hoc.js";
 import { filterSwaggerPropertiesByType } from "../../utility/utility";
@@ -19,189 +19,278 @@ import ApiInformationLink from "../../components/apiInformationLink/apiInformati
 import NotFoundPage from "../error/NotFound.page.jsx";
 
 const ApiInformationPage = () => {
-    const currentuser = useUser();
-    const history = useHistory();
-    if (!currentuser) history.push("/");
-    
-    const { apiId } = useParams();
-    const swaggerHubUrl = `https://api.swaggerhub.com/apis/Hackney/${apiId}`;
-    const apiUrl = `${process.env.REACT_APP_API_URL || `http://${window.location.hostname}:8000/api/v1`}/${apiId}`;
-    const passedParams = useLocation().state || { versions: null, currentVersion: null };
+  const currentuser = useUser();
+  const history = useHistory();
+  if (!currentuser) history.push("/");
 
-    const [swaggerStatus, setSwaggerStatus] = useState({isLoaded: false, error: null });
-    const [apiStatus, setApiStatus] = useState({isLoaded: false, error: null });
+  const { apiId } = useParams();
+  const swaggerHubUrl = `https://api.swaggerhub.com/apis/Hackney/${apiId}`;
+  const apiUrl = `${
+    process.env.REACT_APP_API_URL ||
+    `http://${window.location.hostname}:8000/api/v1`
+  }/${apiId}`;
+  const passedParams = useLocation().state || {
+    versions: null,
+    currentVersion: null,
+  };
 
-    const [versions, setVersions] = useState(passedParams.versions || []);
-    const [currentVersion, setCurrentVersion] = useState(passedParams.currentVersion);
-    const [apiData, setApiData] = useState({});
-    const [swaggerData, setSwaggerData] = useState({});
+  const [swaggerStatus, setSwaggerStatus] = useState({
+    isLoaded: false,
+    error: null,
+  });
+  const [apiStatus, setApiStatus] = useState({ isLoaded: false, error: null });
 
-    const resetState = () => {
-        window.scrollTo(0, 0);
-        setSwaggerStatus({isLoaded: false, error: null });
+  const [versions, setVersions] = useState(passedParams.versions || []);
+  const [currentVersion, setCurrentVersion] = useState(
+    passedParams.currentVersion
+  );
+  const [apiData, setApiData] = useState({});
+  const [swaggerData, setSwaggerData] = useState({});
+
+  const resetState = () => {
+    window.scrollTo(0, 0);
+    setSwaggerStatus({ isLoaded: false, error: null });
+  };
+
+  // Get data from API
+  useEffect(() => {
+    resetState();
+
+    axios
+      .get(apiUrl, {
+        headers: { Authorization: Cookies.get("hackneyToken") },
+      })
+      .then((result) => {
+        setApiData(result.data);
+        setApiStatus({ isLoaded: true, error: null });
+      })
+      .catch((error) => {
+        setApiStatus({ isLoaded: true, error: error });
+      });
+  }, [apiUrl]);
+
+  // Get SwaggerHub data
+  useEffect(() => {
+    const handleSwaggerData = (result) => {
+      setSwaggerData(result);
+      setSwaggerStatus({ isLoaded: true, error: null });
+    };
+    const handleApiVersioning = (result) => {
+      const apiVersions = result.apis.map(
+        (api) =>
+          filterSwaggerPropertiesByType(api.properties, "X-Version").value
+      );
+      setVersions(apiVersions);
+      setCurrentVersion(apiVersions[0]);
+    };
+
+    resetState();
+
+    axios
+      .get(`${swaggerHubUrl}/${currentVersion || ""}`)
+      .then((result) => {
+        currentVersion
+          ? handleSwaggerData(result.data)
+          : handleApiVersioning(result.data);
+      })
+      .catch((error) => {
+        setSwaggerStatus({ isLoaded: true, error: error });
+      });
+  }, [swaggerHubUrl, currentVersion]);
+
+  const formatApiData = () => {
+    const changeVersion = (versionFormatted) => {
+      const version = versionFormatted.replace(
+        /^((\d\.?)*) \[PUBLISHED]/gm,
+        "$1"
+      );
+      setCurrentVersion(version);
+    };
+    const SelectVersion = (
+      <Select
+        name={"VersionNo"}
+        options={versions.map((v) => v.replace(/^\*(.*)/gm, "$1 [PUBLISHED]"))}
+        selectedOption={currentVersion}
+        onChange={changeVersion}
+      />
+    );
+
+    var swaggerLink;
+    const isLoaded = apiStatus.error
+      ? swaggerStatus.isLoaded
+      : apiStatus.isLoaded;
+    if (isLoaded) {
+      const apiName = apiStatus.error
+        ? swaggerData.info.title
+        : apiData.apiName;
+      swaggerLink = (
+        <ApiInformationLink
+          linkText={`${apiName} on SwaggerHub`}
+          url={`${swaggerHubUrl}/${currentVersion}`
+            .replace("api", "app")
+            .replace("/apis", "/apis-docs")}
+        />
+      );
+    } else {
+      swaggerLink = <Skeleton />;
     }
 
-    // Get data from API
-    useEffect(() => {
-        resetState();
-
-        axios.get(apiUrl, {
-            headers: { 'Authorization': Cookies.get('hackneyToken') }
-        }).then((result) => {
-                setApiData(result.data);
-                setApiStatus({ isLoaded: true, error: null });
-            })
-            .catch((error) => {
-                setApiStatus({ isLoaded: true, error: error });
-            });
-    }, [apiUrl]);
-
-    // Get SwaggerHub data
-    useEffect(() => {
-        const handleSwaggerData = (result) => {
-            setSwaggerData(result);
-            setSwaggerStatus({ isLoaded: true, error: null });
-        };
-        const handleApiVersioning = (result) => {
-            const apiVersions = result.apis.map( api =>
-                filterSwaggerPropertiesByType(api.properties, "X-Version").value
-            );
-            setVersions(apiVersions);
-            setCurrentVersion(apiVersions[0]);
-        };
-
-        resetState();
-
-        axios.get(`${swaggerHubUrl}/${currentVersion || ''}`)
-            .then( result => {
-                currentVersion ? handleSwaggerData(result.data) : handleApiVersioning(result.data)
-            })
-            .catch((error) => {
-                setSwaggerStatus({ isLoaded: true, error: error });
-            });
-    }, [swaggerHubUrl, currentVersion]);
-
-
-    const formatApiData = () => {
-
-        const changeVersion = (versionFormatted) => {
-            const version = versionFormatted.replace(/^((\d\.?)*) \[PUBLISHED]/gm, "$1")
-            setCurrentVersion(version);
-        }
-        const SelectVersion = <Select name={"VersionNo"} options={versions.map(v => v.replace(/^\*(.*)/gm, '$1 [PUBLISHED]'))} selectedOption={currentVersion} onChange={changeVersion} />;
-
-        var swaggerLink;
-        const isLoaded = apiStatus.error ? swaggerStatus.isLoaded : apiStatus.isLoaded;
-        if(isLoaded){
-            const apiName = apiStatus.error ? swaggerData.info.title : apiData.apiName;
-            swaggerLink = <ApiInformationLink linkText={`${apiName} on SwaggerHub`} url={`${swaggerHubUrl}/${currentVersion}`.replace("api", "app").replace("/apis", "/apis-docs")} />;
-        } else {
-            swaggerLink = <Skeleton/>
-        }
-
-        var links; var devUrl; var stagingUrl;
-        var applications; var actionLink;
-        if(apiStatus.error){
-            devUrl = stagingUrl = links =  <p>We're having difficulty loading this data.</p>
-        } else {
-            if(apiStatus.isLoaded){
-                links = <ul>
-                            <li><ApiInformationLink linkText={`${apiData.apiName} Specification`} url={apiData.apiSpecificationLink} /></li>
-                            <li><ApiInformationLink linkText={`${apiData.apiName} GitHub Repository`} url={apiData.githubLink} /></li>
-                        </ul>
-                devUrl = <ApiInformationLink linkText={apiData.developmentBaseURL} url={apiData.developmentBaseURL}/>
-                stagingUrl = <ApiInformationLink linkText={apiData.stagingBaseURL} url={apiData.stagingBaseURL}/>
-                // applications = <p {apiData.applications}/>
-                actionLink = <p style={{paddingRight: "3em"}}> edit | delete </p>
-
-            } else {
-                links = <ul>
-                            <li><Skeleton/></li>
-                            <li><Skeleton/></li>
-                            <li><Skeleton/></li>
-                        </ul>
-                devUrl = stagingUrl = <Skeleton/>
-                //applications = <Skeleton/>
-            }
-        }
-
-        TableData.push(
-            ["Version", SelectVersion],
-            ["SwaggerHub Specification", swaggerLink],
-            ["Development Base URL", devUrl],
-            ["Staging Base URL", stagingUrl],
-            ["Relevant Links", links]
+    var links;
+    var devUrl;
+    var stagingUrl;
+    var actionLink;
+    if (apiStatus.error) {
+      devUrl =
+        stagingUrl =
+        links =
+          <p>We're having difficulty loading this data.</p>;
+    } else {
+      if (apiStatus.isLoaded) {
+        links = (
+          <ul>
+            <li>
+              <ApiInformationLink
+                linkText={`${apiData.apiName} Specification`}
+                url={apiData.apiSpecificationLink}
+              />
+            </li>
+            <li>
+              <ApiInformationLink
+                linkText={`${apiData.apiName} GitHub Repository`}
+                url={apiData.githubLink}
+              />
+            </li>
+          </ul>
         );
-
-        // applications.forEach((application) => {
-        //     ApplicationTableData.push([application, actionLink])
-        // });
-
-        applications = apiData.applications
-        // console.log(applications);
-        
-        // for (let value of Object.values(applications)) {
-        //     ApplicationTableData.push(value, actionLink);
-        // }
-
-        ApplicationTableData.push(
-            ["manage my", actionLink ],
-            ["social care", actionLink]
-        )
-    }
-
-    if(swaggerStatus.error && apiStatus.error){
-        if(swaggerStatus.error.response.status === 404 && apiStatus.error.response.status === 404)
-            return <NotFoundPage/>;
-
-        return(
-            <main className="lbh-main-wrapper" id="main-content" role="main">
-                <div id="api-info-page" className="lbh-container">
-                    <Error title="Oops! Something went wrong!" summary={swaggerStatus.error + " | " + apiStatus.error} />
-                </div>
-            </main>
+        devUrl = (
+          <ApiInformationLink
+            linkText={apiData.developmentBaseURL}
+            url={apiData.developmentBaseURL}
+          />
         );
+        stagingUrl = (
+          <ApiInformationLink
+            linkText={apiData.stagingBaseURL}
+            url={apiData.stagingBaseURL}
+          />
+        );
+        actionLink = <p style={{ paddingRight: "3em" }}> edit | delete </p>;
+        apiData.applications.forEach((application) => {
+          var link = (
+            <ApiInformationLink
+              linkText={application.name}
+              url={application.link}
+            />
+          );
+          ApplicationTableData.push([link, actionLink]);
+        });
+      } else {
+        links = (
+          <ul>
+            <li>
+              <Skeleton />
+            </li>
+            <li>
+              <Skeleton />
+            </li>
+            <li>
+              <Skeleton />
+            </li>
+          </ul>
+        );
+        devUrl = stagingUrl = <Skeleton />;
+        //applications = <Skeleton/>
+      }
     }
 
-    const TableData = [];
-    const ApplicationTableData = [];
-    if(!(apiStatus.error && swaggerStatus.error))
-        formatApiData();
+    TableData.push(
+      ["Version", SelectVersion],
+      ["SwaggerHub Specification", swaggerLink],
+      ["Development Base URL", devUrl],
+      ["Staging Base URL", stagingUrl],
+      ["Relevant Links", links]
+    );
+  };
+
+  if (swaggerStatus.error && apiStatus.error) {
+    if (
+      swaggerStatus.error.response.status === 404 &&
+      apiStatus.error.response.status === 404
+    )
+      return <NotFoundPage />;
 
     return (
-        <main className="lbh-main-wrapper" id="main-content" role="main">
-            <div id="api-info-page" className="lbh-container">
-                <div className="sidePanel">
-                    <Breadcrumbs />
-                    <h1>
-                        {!apiStatus.error && (apiData.apiName || <Skeleton/>)}
-                        {(apiStatus.error && (swaggerStatus.isLoaded ? swaggerData.info.title : <Skeleton/>))}
-                    </h1>
-
-                    {!swaggerStatus.isLoaded && <Skeleton/>}
-                    {(swaggerStatus.isLoaded && !swaggerStatus.error) && <EnvironmentTags tags={swaggerData.tags && swaggerData.tags.map(tag =>(tag.name))} />}
-                    {swaggerStatus.error && <EnvironmentTags error={true} />}
-
-                    <p className="lbh-body-m">
-                        {!apiStatus.error && (apiData.description || <Skeleton/>)}
-                        {(apiStatus.error && (swaggerStatus.isLoaded ? swaggerData.info.description : <Skeleton/>))}
-                    </p>
-                </div>
-                <div className="main-container table-container">
-                        <span className="govuk-caption-xl lbh-caption">API Information</span>
-                        <hr/>
-                        <Table tableData={TableData} />
-                        <span className="govuk-caption-xl lbh-caption">Applications consumed by API</span>
-                        <hr/>
-                        <div className="column-2">
-                        <ApplicationTable tableData={ApplicationTableData} />
-                        </div>
-                        {swaggerStatus.error && <Error title="Oops! Something went wrong!" summary={swaggerStatus.error.message} />}
-                        {apiStatus.error && <Error title="Oops! Something went wrong!" summary={apiStatus.error.message} />}
-                </div>
-            </div>
-        </main>
+      <main className="lbh-main-wrapper" id="main-content" role="main">
+        <div id="api-info-page" className="lbh-container">
+          <Error
+            title="Oops! Something went wrong!"
+            summary={swaggerStatus.error + " | " + apiStatus.error}
+          />
+        </div>
+      </main>
     );
+  }
+
+  const TableData = [];
+  const ApplicationTableData = [];
+  if (!(apiStatus.error && swaggerStatus.error)) formatApiData();
+
+  return (
+    <main className="lbh-main-wrapper" id="main-content" role="main">
+      <div id="api-info-page" className="lbh-container">
+        <div className="sidePanel">
+          <Breadcrumbs />
+          <h1>
+            {!apiStatus.error && (apiData.apiName || <Skeleton />)}
+            {apiStatus.error &&
+              (swaggerStatus.isLoaded ? swaggerData.info.title : <Skeleton />)}
+          </h1>
+
+          {!swaggerStatus.isLoaded && <Skeleton />}
+          {swaggerStatus.isLoaded && !swaggerStatus.error && (
+            <EnvironmentTags
+              tags={swaggerData.tags && swaggerData.tags.map((tag) => tag.name)}
+            />
+          )}
+          {swaggerStatus.error && <EnvironmentTags error={true} />}
+
+          <p className="lbh-body-m">
+            {!apiStatus.error && (apiData.description || <Skeleton />)}
+            {apiStatus.error &&
+              (swaggerStatus.isLoaded ? (
+                swaggerData.info.description
+              ) : (
+                <Skeleton />
+              ))}
+          </p>
+        </div>
+        <div className="main-container table-container">
+          <span className="govuk-caption-xl lbh-caption">API Information</span>
+          <hr />
+          <Table tableData={TableData} />
+          <span className="govuk-caption-xl lbh-caption">
+            Applications consumed by API
+          </span>
+          <hr />
+          <div className="column-2">
+            <ApplicationTable tableData={ApplicationTableData} />
+          </div>
+          {swaggerStatus.error && (
+            <Error
+              title="Oops! Something went wrong!"
+              summary={swaggerStatus.error.message}
+            />
+          )}
+          {apiStatus.error && (
+            <Error
+              title="Oops! Something went wrong!"
+              summary={apiStatus.error.message}
+            />
+          )}
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default withUser(ApiInformationPage);
