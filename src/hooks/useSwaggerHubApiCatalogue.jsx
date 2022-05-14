496001cb5b7690984e9930778a59f23e805f1b50
AtiyyahRawat <atiyyah.rawat@gmail.com>
2022-05-14 23:31:04 +0100

    Refactor API Catalogue Page

---
+++ src/hooks/useSwaggerHubApiCatalogue.jsx
@@ -0,0 +1,122 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+
+function useSwaggerHubApiCatalogue(searchQuery) {
+    // From SwaggerHub Registry API Documentation
+    // Map visual values to query params for API
+    const queryParamsOptions = {
+        state: {
+            "All APIs": "ALL",
+            "Live APIs": "PUBLISHED",
+            "APIs in development": "UNPUBLISHED",
+        },
+        sort: {
+            "Last Modified": { sort: "UPDATED", order: "DESC" },
+            "A-Z": { sort: "TITLE", order: "ASC" },
+            "Z-A": { sort: "TITLE", order: "DESC" },
+            ...(searchQuery && { "Relevance": "BEST_MATCH" })
+
+        }
+    }
+
+    const [status, setStatus] = useState({
+        isLoaded: false,
+        error: null
+    });
+
+    const [data, setData] = useState({
+        apis: [],
+        offset: 0
+    });
+
+    const [queryParams, setQueryParams] = useState({
+        specType: "API",
+        owner: "Hackney",
+        page: 0,
+        limit: 5,
+        state: "ALL",
+        sort: (searchQuery ? "BEST_MATCH" : "UPDATED"),
+        order: "DESC",
+        ...(searchQuery && { query: searchQuery })
+    });
+
+    useEffect(() => {
+        function reset() {
+            setStatus({ isLoaded: false, error: null });
+            setData({ apis: [], offset: 0 });
+        }
+
+        function parseQueryParams() {
+            return Object.keys(queryParams)
+                .map(x => x + '=' + queryParams[x])
+                .join('&');
+        }
+
+        reset();
+
+        axios.get(`https://api.swaggerhub.com/specs?${parseQueryParams()}`)
+            .then((result) => {
+                var data = result.data;
+                if (data.totalCount === 0) {
+                    setStatus({
+                        isLoaded: true,
+                        error: { code: "No results", message: "No results found." }
+                    });
+                } else {
+                    setData(data);
+                    setStatus({ isLoaded: true, error: false });
+                }
+            })
+            .catch((error) => {
+                setStatus({ isLoaded: true, error: error });
+            })
+    }, [queryParams, searchQuery]);
+
+    // public functions
+
+    const getFormattedParamOptions = (paramName) => {
+        const parameterOptions = queryParamsOptions[paramName];
+        return Object.keys(parameterOptions);
+    }
+
+    const formatSelectedParam = (paramName) => {
+        const selectedValue = queryParams[paramName]; // currently selected e.g. published
+        const formattedOptions = queryParamsOptions[paramName]; // possible options
+
+        return Object.keys(formattedOptions)
+            .find(key => formattedOptions[key] === selectedValue);
+
+    }
+
+    const getPaginationData = () => {
+        return {
+            limit: queryParams.limit,
+            offset: data.offset,
+            totalCount: data.totalCount
+        }
+    }
+
+    const updatePagination = (newPage) => {
+        setQueryParams({ ...queryParams, page: newPage });
+    }
+
+    const updateApiFilter = (newApiFilter) => {
+        setQueryParams({
+            ...queryParams,
+            state: queryParamsOptions.state[newApiFilter],
+            page: 0
+        });
+    }
+
+    const updateSortBy = (sortBy) => {
+        setQueryParams({
+            ...queryParams,
+            ...queryParamsOptions.sort[sortBy],
+            page: 0
+        });
+    }
+
+    return [status, data, [getFormattedParamOptions, getPaginationData, formatSelectedParam], [updatePagination, updateApiFilter, updateSortBy]];
+}
+
+export default useSwaggerHubApiCatalogue;
--- src/pages/apiCatalogue/apiCatalogue.page.jsx
+++ src/pages/apiCatalogue/apiCatalogue.page.jsx
@@ -1,6 +1,7 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import { useLocation } from "react-router-dom";
 import withUser from "../../HOCs/with-user.hoc.js";
+import useSwaggerHubApiCatalogue from "../../hooks/useSwaggerHubApiCatalogue.jsx";
 
 import ApiPreview from "../../components/apiPreview/apiPreview.component";
 import Breadcrumbs from "../../components/breadcrumbs/breadcrumbs.component";
@@ -16,110 +17,54 @@ const ApiCataloguePage = () => {
   let location = useLocation();
   const searchQuery = decodeURIComponent(location.search.replace("?search=", "").replaceAll("+", " "));
 
-  const [error, setError] = useState(null);
-  const [isLoaded, setIsLoaded] = useState(false);
-  const [apis, setApis] = useState([]);
-  const [apiMetadata, setApiMetadata] = useState({
-    "offset": 0
-  });
-  const [queryParams, setQueryParams] = useState({
-    specType: "API",
-    owner: "Hackney",
-    page: 0,
-    limit: 5,
-    state: "ALL",
-    sort: (searchQuery ? "BEST_MATCH" : "UPDATED"),
-    order: "DESC"
-  });
+  const [apiStatus, apiData, utilties, functions] = useSwaggerHubApiCatalogue(searchQuery);
+  const [getFormattedParamOptions, getPaginationData, formatSelectedParam] = utilties;
+  const [updatePagination, updateApiFilter, updateSortBy] = functions;
 
-  const resetState = () => {
-    setError(null);
-    setIsLoaded(false);
-    setApis([]);
-  }
-
-  const apiParamsOptions = {
-    // From SwaggerHub Registry API Documentation
-    state: {
-      "All APIs": "ALL",
-      "Live APIs": "PUBLISHED",
-      "APIs in development": "UNPUBLISHED",
-    },
-    sort: {
-      "Last Modified": { sort: "UPDATED", order: "DESC" },
-      "A-Z": { sort: "TITLE", order: "ASC" },
-      "Z-A": { sort: "TITLE", order: "DESC" },
-      ...(searchQuery && { "Relevance": "BEST_MATCH" })
-    }
-  }
-
-  const formatApiParam = (value, param) => {
-    return Object.keys(apiParamsOptions[value]).find(key => JSON.stringify(apiParamsOptions[value][key]) === JSON.stringify(param));
-  }
-  const updatePagination = (newPage) => {
-    setQueryParams({...queryParams, page: newPage});
-  }
-  const updateApiFilter = (newApiFilter) => {
-    setQueryParams({...queryParams, state: apiParamsOptions.state[newApiFilter], page: 0 });
-    // reset pagination when switching filters
-  }
-  const updateSortBy = (sortBy) => {
-    setQueryParams({...queryParams, ...apiParamsOptions.sort[sortBy], page: 0 });
-  }
-
-  useEffect(() => {
-    const parseQueryParams = () => {
-      return Object.keys(queryParams).map(key => key + '=' + queryParams[key]).join('&');
-    }
-    
-    resetState();
-
-    fetch(`https://api.swaggerhub.com/specs?${parseQueryParams()}${ searchQuery? `&query=${searchQuery}`: ""}`)
-      .then(res => res.json())
-      .then((result) => {
-        if(result.totalCount === 0){
-          setError({ code: "No results", message: "No results found." })
-        }
-        setApis(result.apis);
-        setApiMetadata(result);
-        setIsLoaded(true);
-      })
-      .catch((error) => {
-        setError(error);
-        setIsLoaded(true);
-      });
-  }, [queryParams, searchQuery]);
-
-  const radioData = {
+  const isPublishedFilterConfig = {
     legend: "Filter By",
     formName: "filterApis",
-    values: Object.keys(apiParamsOptions.state)
+    values: getFormattedParamOptions("state")
   }
 
-  return(
+  return (
     <main className="lbh-main-wrapper" id="apis-page" role="main">
       <div className="lbh-container">
-        <Breadcrumbs/>
+        <Breadcrumbs />
         <div className="heading">
-          {searchQuery? <h1>Search for "{searchQuery}"</h1> : <h1>API Catalogue</h1>}
-          <Search id={"search"} placeholder={searchQuery? "Search again..." : "Search for an API..."} />
+          {searchQuery ? <h1>Search for "{searchQuery}"</h1> : <h1>API Catalogue</h1>}
+          <Search id={"search"} placeholder={searchQuery ? "Search again..." : "Search for an API..."} />
         </div>
-        <BackToTop href="#header"/>
-        <Radios onChange={updateApiFilter} {...radioData}/>
+
+        <BackToTop href="#header" />
+        <Radios onChange={updateApiFilter} {...isPublishedFilterConfig} />
         <Details summary={"Advanced..."}>
-          <Select name={"SortBy"} label={"Sort by:"} options={Object.keys(apiParamsOptions.sort)} selectedOption={formatApiParam("sort", queryParams.sort)} onChange={updateSortBy} />
+          <Select
+            name={"SortBy"}
+            label={"Sort by:"}
+            options={getFormattedParamOptions("sort")}
+            selectedOption={formatSelectedParam("sort")}
+            onChange={updateSortBy}
+          />
         </Details>
-        { !isLoaded && <h3>Loading..</h3> }
-        { error && <Error title={error.code !== "No results" && "Oops! Something went wrong!"} summary={error.message} className={error.code === "No results" && "secondary"}/> }
-        { isLoaded && !error &&
-            <>
-              <Pagination onChange={updatePagination} limit={queryParams.limit} {...apiMetadata} />
-              <ul id="apisList">
-                {apis.map((item, index) => (
-                  < ApiPreview key={index} {...item} />
-                ))}
-              </ul>
-            </>
+
+        {!apiStatus.isLoaded && <h3>Loading..</h3>}
+        {apiStatus.error &&
+          <Error
+            title={apiStatus.error.code !== "No results" && "Oops! Something went wrong!"}
+            summary={apiStatus.error.message}
+            className={apiStatus.error.code === "No results" && "secondary"}
+          />
+        }
+        {apiStatus.isLoaded && !apiStatus.error &&
+          <>
+            <Pagination onChange={updatePagination} {...getPaginationData()} />
+            <ul id="apisList">
+              {apiData.apis.map((item, index) => (
+                < ApiPreview key={index} {...item} />
+              ))}
+            </ul>
+          </>
         }
       </div>
     </main>
