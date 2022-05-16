1402a1ea5fd4648dacd8083060f66f51b94b62e4
LiviaBas <livia.basiu@gmail.com>
2022-05-16 14:33:53 +0100

    zoom feature implemented
    zoom in and zoom out feature implemented

--- src/pages/home-pages/home-pages.pages.jsx
+++ src/pages/home-pages/home-pages.pages.jsx
@@ -3,6 +3,7 @@ import arn from "../../assets/arn.png";
 import apiflow from "../../assets/apiflow.png";
 import ssogroups from "../../assets/ssogroups.png";
 import MyCollapsible from "../../components/accordion/accordion.jsx";
+import PinchZoomPan from "react-responsive-pinch-zoom-pan";
 
 export const HomePage1 = () => {
   return (
@@ -201,8 +202,14 @@ export const HomePage6 = () => {
   return (
     <div id="how-the-authentication-works" className="main-container">
       <h2>How the API Authentication process works </h2>
-      <div>
+      <h3>Zoom</h3>
+      <div style={{ width: '900px', height: '500px' }} className="zoom-feature">
+        <PinchZoomPan>
         <img src={apiflow} alt="" className="center"></img>
+        </PinchZoomPan>
+      </div>
+
+      <div className="content">
         <p>
           When a consumer makes a request to an API, the consumer provide their
           JWT access token in the authorization header of the request. Our
@@ -219,7 +226,6 @@ export const HomePage6 = () => {
           deployed. This is so the API ID received as part of the request can be
           mapped to the actual API name.
         </p>
-      </div>
 
       <p>
         We would like to reuse the auth token set by the frontend authentication
@@ -254,6 +260,7 @@ export const HomePage6 = () => {
         Furthermore, this business is empowered to manage their own resources
         and determine who has access to them which gives them more flexibility.
       </p>
+    </div>
     </div>
   );
 };
