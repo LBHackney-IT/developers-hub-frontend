f3e99b94a15dba72e7b70ed9f6c5450b0153fe91
humulla <43747248+humulla@users.noreply.github.com>
2022-05-26 16:18:50 +0100

    Updated contact us page

--- src/components/contact-container/contact-data.js
+++ src/components/contact-container/contact-data.js
@@ -1,10 +1,8 @@
 const CONTACT_DATA = [
-  "Rashmi Shetty[rashmi.shetty@hackney.gov.uk]- Product Owner of Platform APIs",
+  "Rashmi Shetty [rashmi.shetty@hackney.gov.uk]- Product Owner of Platform APIs",
   "Mirela Georgieva[mirela.georgieva@hackney.gov.uk]-Lead Developer",
   "Selwyn Preston[selwyn.preston@hackney.gov.uk]-Lead Developer",
-  "Matthew Keyworth[matthew.keyworth@hackney.gov.uk]-Lead Developer",
   "Dave Dean[dave.dean@hackney.gov.uk]-Lead Developer",
-  "Bukky Omotoso[Bukky.Omotoso@hackney.gov.uk]-Lead Developer",
   "Livia Basiu[livia.basiu@hackney.gov.uk]-Maintainer and Collaborator",
   "Atiyyah Rawat[atiyyah.rawat@hackney.gov.uk]-Maintainer and Collaborator",
   "Humairaa Mulla[Humairaa.Mulla@hackney.gov.uk]-Maintainer and Collaborator",
